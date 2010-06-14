3dffb1f8cac5a80b9c963e962207fd021394aa56
Rich Downie <rich@richdownie.com>
2010-06-14 09:57:18 -0400

    updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
   def index
   end
-
+  
+  def dump
+    Result.create(:title => @testing_title)
+  end
+  
+  # def update
+  #   @user.password = params[:user][:password]
+  #   @user.password_confirmation = params[:user][:password_confirmation]
+  #   if @user.save
+  #     flash[:notice] = "Password successfully updated"
+  #     redirect_to account_path
+  #   else
+  #     render :action => :edit
+  #   end
+  # end
 end
