40155c6354477491546e13115a31a820f58fb99e
Rich Downie <rich@richdownie.com>
2010-06-17 21:41:54 -0400

    formatted view

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,7 +3,7 @@ class ResultsController < ApplicationController
   # GET /results.xml
   def index
     @results = Result.find(:all, :order => "created_at DESC")
-
+    
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @results }
