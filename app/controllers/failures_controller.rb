6a714ad2cf66ba2d5e99ac26f42270268fccdbc1
Rich Downie <rich@richdownie.com>
2010-07-02 11:11:50 -0400

    adding failures

--- app/controllers/failures_controller.rb
+++ app/controllers/failures_controller.rb
@@ -7,6 +7,14 @@ class FailuresController < ApplicationController
     @issue_admin_feature = Result.issue_admin_feature.count
     @issue_aging_feature = Result.issue_aging_feature.count
     @issue_bulk_delete_feature = Result.issue_bulk_delete_feature.count
+    @issue_bulk_delete_by_creator_feature = Result.issue_bulk_delete_by_creator_feature.count
+    @issue_custom_fields_feature = Result.issue_custom_fields_feature.count
+    @issue_dashboard_feature = Result.issue_dashboard_feature.count
+    @issue_email_feature = Result.issue_email_feature.count
+    @issue_import_feature = Result.issue_import_feature.count
+    
+    
+    
     @issue_search_feature = Result.issue_search_feature.count
   end
 
--- app/models/result.rb
+++ app/models/result.rb
@@ -5,8 +5,15 @@ class Result < ActiveRecord::Base
   
   
   
-  named_scope :issue_bulk_delete_feature, :conditions => ['title LIKE ?', '%issue_bulk_delete.feature%']
-  named_scope :issue_aging_feature, :conditions => ['title LIKE ?', '%issue_aging.feature%']
   named_scope :issue_admin_feature, :conditions => ['title LIKE ?', '%issue_admin.feature%']
+  named_scope :issue_aging_feature, :conditions => ['title LIKE ?', '%issue_aging.feature%']
+  named_scope :issue_bulk_delete_feature, :conditions => ['title LIKE ?', '%issue_bulk_delete.feature%']
+  named_scope :issue_bulk_delete_by_creator_feature, :conditions => ['title LIKE ?', '%issue_bulk_delete_by_creator.feature%']
+  named_scope :issue_custom_fields_feature, :conditions => ['title LIKE ?', '%issue_custom_fields.feature%']
+  named_scope :issue_dashboard_feature, :conditions => ['title LIKE ?', '%issue_dashboard.feature%']
+  named_scope :issue_email_feature, :conditions => ['title LIKE ?', '%issue_email.feature%']
+  named_scope :issue_import_feature, :conditions => ['title LIKE ?', '%issue_import.feature%']
+  
+  
   named_scope :issue_search_feature, :conditions => ['title LIKE ?', '%issue_search.feature%']
 end
