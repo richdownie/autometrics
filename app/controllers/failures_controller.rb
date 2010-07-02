011fe06eeb61dfd5b5fda260ae7d95b79f08242e
Rich Downie <rich@richdownie.com>
2010-07-02 11:00:24 -0400

    adding failures

--- app/controllers/failures_controller.rb
+++ app/controllers/failures_controller.rb
@@ -1,6 +1,8 @@
 class FailuresController < ApplicationController
   def index
     @item_feature = Result.item_feature.count
+    @activities_feature = Result.activities_feature.count
+    @issue_search_feature = Result.issue_search_feature.count
   end
 
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,6 @@
 class Result < ActiveRecord::Base
   
   named_scope :item_feature, :conditions => ['title LIKE ?', '%item.feature%']
+  named_scope :activities_feature, :conditions => ['title LIKE ?', '%activities.feature%']
+  named_scope :issue_search_feature, :conditions => ['title LIKE ?', '%issue_search.feature%']
 end
