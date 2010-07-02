88164857765bfd52cdf5224594afea7074b25fbb
Rich Downie <rich@richdownie.com>
2010-07-02 11:04:46 -0400

    adding failures

--- app/controllers/failures_controller.rb
+++ app/controllers/failures_controller.rb
@@ -2,6 +2,11 @@ class FailuresController < ApplicationController
   def index
     @item_feature = Result.item_feature.count
     @activities_feature = Result.activities_feature.count
+    
+    
+    @issue_admin_feature = Result.issue_admin_feature.count
+    @issue_aging_feature = Result.issue_aging_feature.count
+    @issue_bulk_delete_feature = Result.issue_bulk_delete_feature.count
     @issue_search_feature = Result.issue_search_feature.count
   end
 
--- app/models/result.rb
+++ app/models/result.rb
@@ -2,5 +2,11 @@ class Result < ActiveRecord::Base
   
   named_scope :item_feature, :conditions => ['title LIKE ?', '%item.feature%']
   named_scope :activities_feature, :conditions => ['title LIKE ?', '%activities.feature%']
+  
+  
+  
+  named_scope :issue_bulk_delete_feature, :conditions => ['title LIKE ?', '%issue_bulk_delete.feature%']
+  named_scope :issue_aging_feature, :conditions => ['title LIKE ?', '%issue_aging.feature%']
+  named_scope :issue_admin_feature, :conditions => ['title LIKE ?', '%issue_admin.feature%']
   named_scope :issue_search_feature, :conditions => ['title LIKE ?', '%issue_search.feature%']
 end
