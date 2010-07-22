befdbad3f64dcb46a463ef520e006cde4ca87559
Rich Downie <rich@richdownie.com>
2010-07-22 14:53:52 -0400

    updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def my_time_format
+  def my_time_format(date)
     strftime("%m/%d/%Y")
   end
 end
