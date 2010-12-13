bad7b3f20b4d77e3490a19fb0ac46953d7f1244b
Rich Downie <rich@richdownie.com>
2010-12-13 10:04:26 -0500

    adding destory to manual steps

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -17,4 +17,11 @@ class ManualsController < ApplicationController
       flash[:notice] = 'Could not create new scenario'
     end
   end
+  
+  def destroy
+    @manual = Manual.find(params[:id])
+    @manual.destroy
+    flash[:notice] = 'Deleted Manual Scenario'
+    redirect_to manuals_path
+  end
 end
