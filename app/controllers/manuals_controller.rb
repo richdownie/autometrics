0dbb95341b976872b559e0cc872574460762b8f9
Rich Downie <rich@richdownie.com>
2010-12-20 23:40:42 -0500

    not needed

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -33,7 +33,6 @@ before_filter :authorize, :only => [:index]
   
   def edit
     @manual = Manual.find(params[:id])
-    @link = "https://support.rollstream.com/browse/DEV-"
   end
   
   def update
